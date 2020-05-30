aa02782eacd5c56079f9327aa78cc9c9e684ee2e
scorpion <vaibhav.159@gmail.com>
2020-05-31 03:03:57 +0530

    this is my first final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ // advice api  {using API IN REACT}
+import React, { Component } from 'react'
+import axios from 'axios';
+import './assests/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {advise: ""};
+
+// function calling 
+componentDidMount() {
+  // console.log('compoenent did mount')
+  this.fetchAdvice();
+}
+  
+// fetching the advice 
+
+// not using const bec. it is a fucntion belonging to class
+fetchAdvice  = () => {
+  axios.get('https://api.adviceslip.com/advice')
+  // using .then() and .catch we can get the response in the way we like 
+            .then((response) => {
+              
+                // console.log(response.data.slip.advice);
+              // *if wirtten wrong name it will reurn undefined 
+      
+              // *destrucuring advice 
+              // *note we write that value in {} that we want and rest address we assign it 
+              const { advice } = response.data.slip;
+              console.log(advice);
+              this.setState({advise: advice});
+
+              })
+  
+            .catch((error) => {
+                console.log("kuch to gadbad hai daya");
+            });
+  // if api is corrent then this is executed else catch is executed 
+
 }
 
-export default App;
+  render() { 
+    // console.log("HIIIIIIIIIIIIIIIII");
+    // console.log(this.state);
+    return (
+        <div className="app">
+          <dic className="card">
+          <h1 className="heading">{this.state.advise}</h1>
+          <button className="button" onClick={this.fetchAdvice}>Push me</button>
+          </dic>
+        </div>
+    )
+  }
+}
+
+
+// to call a fucntion from button use this.fetchAdvice
+
+
